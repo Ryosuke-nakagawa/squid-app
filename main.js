5d92b96dab3493958bf97c785dd2fc55c00e958f
Ryosuke <tffcd0514@gmail.com>
2022-07-07 22:10:22 +0900

    add: vimでイカを動かす #2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const D_WIDTH = 480;
-const D_HEIGHT = 320;
+const D_WIDTH = 1400;
+const D_HEIGHT = 700;
 
 // 1, Phaser3の設定データ
 const config = {
@@ -30,12 +30,36 @@ let phaser = new Phaser.Game(config);
 
 function preload(){
 	console.log("preload!!");
+    this.load.image("into_sea", "./assets/into_sea.png");
+    this.load.image("squid", "./assets/squid.png");
+    this.load.image("ground", "./assets/ground.png");
 }
 
 function create(){
 	console.log("create!!");
+    this.add.image(D_WIDTH/2, D_HEIGHT/2, "into_sea")
+    player = this.physics.add.sprite(240, 80, "squid")
+    let staticGroup = this.physics.add.staticGroup();// 動かない物体をまとめる
+	staticGroup.create(D_WIDTH/2, D_HEIGHT-32, "ground");// 地面
+	this.physics.add.collider(player, staticGroup);// 衝突処理を設定する
 }
 
 function update(){
 	console.log("update!!");
+    let cursors = this.input.keyboard.createCursorKeys();
+    let keyJ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.J);
+    let keyH = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.H);
+    let keyL = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.L);
+	if(cursors.up.isDown || keyJ.isDown){
+		//console.log("Up!!");
+		player.setVelocityY(-200);// 上方向の速度を設定
+	}else if(cursors.left.isDown || keyH.isDown){
+		//console.log("Left");
+		player.setVelocityX(-200);// 左方向の速度を設定
+	}else if(cursors.right.isDown || keyL.isDown){
+		//console.log("Right!!");
+		player.setVelocityX(200);// 右方向の速度を設定
+	}else{
+		player.setVelocityX(0);// 横方向の速度を0
+	}
 }
